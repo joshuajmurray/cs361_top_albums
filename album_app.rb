e8ab65765912b73d88aaddd6d19ffb2dd2d225aa
joshua murray <joshuajmurray@gmail.com>
2018-02-14 15:16:32 -0800

    Add splits to album data and instert into array as mixed types for sorting

--- album_app.rb
+++ album_app.rb
@@ -4,21 +4,15 @@ class AlbumApp
   	req = Rack::Request.new(env)
   	path = req.path
     if '/albums'==path
-      simpleArrayTest = Array.new
+      albumArray = Array.new
       response_body = "<h1>Top 100 Albums of All Time</h1>"
-      File.open("top_100_albums.txt").each do |line| 
-        # response_body << line.to_s
-        simpleArrayTest << line.to_s
+      File.open("top_100_albums.txt").each_with_index do |line,i|
+        albumArray << [i + 1,  line.to_s.split(", ")[0], line.to_s.split(", ")[1].chomp.to_i]
       end
 
-      for albums in simpleArrayTest
-        response_body << albums
+      albumArray.each do |album|
+        response_body << album.to_s
       end
-      # # Read the data from the file.
-      # albums = ['fee', 'fi', 'fo', 'funk']
-      # # Append it to the response body.
-      # response_body << albums.to_s
-      # Send the response
       [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
     else
       [404, {'Content-Type' => 'text/html'}, ["Uknown URL"]]
