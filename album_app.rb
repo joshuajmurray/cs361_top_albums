c97f8273b51d77bc0a9b5f05896ce951c7bdbf35
joshua murray <joshuajmurray@gmail.com>
2018-02-14 14:27:54 -0800

    Add url parsing for /albums

--- album_app.rb
+++ album_app.rb
@@ -1,13 +1,19 @@
 class AlbumApp
 
   def call(env)
-    response_body = "<h1>Top 100 Albums of All Time</h1>"
-    # Read the data from the file.
-    albums = ['fee', 'fi', 'fo', 'funk']
-    # Append it to the response body.
-    response_body << albums.to_s
-    # Send the response
-    [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
-  end
+  	req = Rack::Request.new(env)
+  	path = req.path
+    if '/albums'==path
 
+      response_body = "<h1>Top 100 Albums of All Time</h1>"
+      # Read the data from the file.
+      albums = ['fee', 'fi', 'fo', 'funk']
+      # Append it to the response body.
+      response_body << albums.to_s
+      # Send the response
+      [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
+    else
+      [404, {'Content-Type' => 'text/html'}, ["Uknown URL"]]
+    end
+  end
 end
