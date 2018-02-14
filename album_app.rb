14c3131904f4f4a7c7bb3e61cf4b22e017760f1f
joshua murray <joshuajmurray@gmail.com>
2018-02-14 14:45:30 -0800

    Add array to hold file input data

--- album_app.rb
+++ album_app.rb
@@ -4,12 +4,20 @@ class AlbumApp
   	req = Rack::Request.new(env)
   	path = req.path
     if '/albums'==path
-
+      simpleArrayTest = Array.new
       response_body = "<h1>Top 100 Albums of All Time</h1>"
-      # Read the data from the file.
-      albums = ['fee', 'fi', 'fo', 'funk']
-      # Append it to the response body.
-      response_body << albums.to_s
+      File.open("top_100_albums.txt").each do |line| 
+        # response_body << line.to_s
+        simpleArrayTest << line.to_s
+      end
+
+      for albums in simpleArrayTest
+        response_body << albums
+      end
+      # # Read the data from the file.
+      # albums = ['fee', 'fi', 'fo', 'funk']
+      # # Append it to the response body.
+      # response_body << albums.to_s
       # Send the response
       [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
     else
