8f3518d08f554297b147032b1395a7d608c946b8
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-02-14 14:51:25 -0800

    Add html head and html tail

--- album_app.rb
+++ album_app.rb
@@ -4,13 +4,14 @@ class AlbumApp
   	req = Rack::Request.new(env)
   	path = req.path
     if '/albums'==path
-
-      response_body = "<h1>Top 100 Albums of All Time</h1>"
-      # Read the data from the file.
+      response_head = "<!DOCTYPE HTML><html><head><title>Top 100 Albums</title></head><body>"
+      response_body << response_head.to_s
+      
       albums = ['fee', 'fi', 'fo', 'funk']
-      # Append it to the response body.
       response_body << albums.to_s
-      # Send the response
+
+      response_tail = "</body></html>"
+      response_body << response_tail
       [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
     else
       [404, {'Content-Type' => 'text/html'}, ["Uknown URL"]]
