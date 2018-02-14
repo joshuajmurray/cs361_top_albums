6fffc8b6a16985cff536467409377d5fda6dc8d1
joshua murray <joshuajmurray@gmail.com>
2018-02-14 15:56:49 -0800

    Add sort methods and path calls for each

--- album_app.rb
+++ album_app.rb
@@ -2,15 +2,24 @@ class AlbumApp
 
   def call(env)
   	request = Rack::Request.new(env)
-    if '/albums' == request.path
+    if request.path.include? '/albums'
       response_body = "<!DOCTYPE HTML><html><head><title>Top 100 Albums</title></head><body>"
       albumArray = Array.new
+      albumArrayOut = Array.new
       response_body << "<h1>Top 100 Albums of All Time</h1>"
       File.open("top_100_albums.txt").each_with_index do |line,i|
-        albumArray << [i + 1,  line.to_s.split(", ")[0], line.to_s.split(", ")[1].chomp.to_i]
+        albumArray << { rank: i + 1,  title: line.to_s.split(", ")[0], year: line.to_s.split(", ")[1].chomp.to_i }
       end
 
-      albumArray.each do |album|
+      if '/albums/title' == request.path
+        albumArrayOut = albumArray.sort_by { |o| o[:title] }
+        puts "title sort"
+      elsif '/albums/year' == request.path
+        albumArrayOut = albumArray.sort_by { |o| o[:year] }
+        puts "year sort"
+      end
+
+      albumArrayOut.each do |album|
         response_body << album.to_s
       end
 
