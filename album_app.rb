2d4eb5d5d8da239c38a782c56305b24f6944ec62
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-02-26 15:11:42 -0800

    Add base html. References #6.

--- album_app.rb
+++ album_app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  "hello world"
+  erb :index
 end
 
 get '/title' do
--- config.ru
+++ config.ru
@@ -1,3 +1,4 @@
 require_relative 'album_app'
+require_relative 'album_list'
 
 run AlbumApp.new
